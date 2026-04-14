a40e45899c1c4f4ce23de741c22449d957d17f07
hasebe <c0a2520266@edu.teu.ac.jp>
2026-04-14 15:16:53 +0900

    練習3と4:こうかとん画像の描画

---
+++ pygame_image.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    tmr = 0
+
+    kouka_img = pg.image.load("fig/3.png")
+    kouka_img = pg.transform.flip(kouka_img , True, False)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kouka_img, [300, 200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(1)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
